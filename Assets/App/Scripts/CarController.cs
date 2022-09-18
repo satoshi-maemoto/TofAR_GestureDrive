61b81fc60552dd02e9c4105c420e78c670d7c0f0
satoshi-maemoto <peugeot.106.s16@gmail.com>
2022-09-18 14:13:25 +0900

    Adjustment for rear camera

--- Assets/App/Scripts/CarController.cs
+++ Assets/App/Scripts/CarController.cs
@@ -27,27 +27,53 @@ namespace TofArGestureDrive
 
         private IEnumerator Process()
         {
+            var useFrontCamera = TofArHandManager.Instance.RecogMode == RecogMode.Face2Face;
             while (true)
             {
                 // カメラから手の中心までの距離を推進力に変換 0(m)->100% maxDistance(m)m->0%
-                // Frontカメラの場合、手の左右は逆転する
-                var rPower = 0f;
-                if ((this.LHand.handStatus == HandStatus.BothHands) && (this.LHand.HandPoints?.Length > 0))
+                if (useFrontCamera)
                 {
-                    var handDistance = this.LHand.HandPoints[(int)HandPointIndex.HandCenter].z;
-                    rPower = Mathf.Max(0,  this.maxDistance - handDistance);
-                }
-                this.RTire.AddForce(this.RTire.transform.forward * rPower * this.forceFactor);
-                this.ShowAccelePower(this.RPowerText, rPower);
+                    // Frontカメラの場合、手の左右は逆転する
+                    var rPower = 0f;
+                    if ((this.LHand.handStatus == HandStatus.BothHands) && (this.LHand.HandPoints?.Length > 0))
+                    {
+                        var handDistance = this.LHand.HandPoints[(int)HandPointIndex.HandCenter].z;
+                        rPower = Mathf.Max(0, this.maxDistance - handDistance);
+                    }
+                    this.RTire.AddForce(this.RTire.transform.forward * rPower * this.forceFactor);
+                    this.ShowAccelePower(this.RPowerText, rPower);
 
-                var lPower = 0f;
-                if ((this.RHand.handStatus == HandStatus.BothHands) && (this.RHand.HandPoints?.Length > 0))
-                {
-                    var handDistance = this.RHand.HandPoints[(int)HandPointIndex.HandCenter].z;
-                    lPower = Mathf.Max(0, this.maxDistance - handDistance);
+
+                    var lPower = 0f;
+                    if ((this.RHand.handStatus == HandStatus.BothHands) && (this.RHand.HandPoints?.Length > 0))
+                    {
+                        var handDistance = this.RHand.HandPoints[(int)HandPointIndex.HandCenter].z;
+                        lPower = Mathf.Max(0, this.maxDistance - handDistance);
+                    }
+                    this.LTire.AddForce(this.LTire.transform.forward * lPower * this.forceFactor);
+                    this.ShowAccelePower(this.LPowerText, lPower);
                 }
-                this.LTire.AddForce(this.LTire.transform.forward * lPower * this.forceFactor);
-                this.ShowAccelePower(this.LPowerText, lPower);
+                else
+                {
+                    var rPower = 0f;
+                    if ((this.LHand.handStatus == HandStatus.BothHands) && (this.LHand.HandPoints?.Length > 0))
+                    {
+                        var handDistance = this.LHand.HandPoints[(int)HandPointIndex.HandCenter].z;
+                        rPower = Mathf.Min(this.maxDistance, handDistance);
+                    }
+                    this.LTire.AddForce(this.LTire.transform.forward * rPower * this.forceFactor);
+                    this.ShowAccelePower(this.LPowerText, rPower);
+
+
+                    var lPower = 0f;
+                    if ((this.RHand.handStatus == HandStatus.BothHands) && (this.RHand.HandPoints?.Length > 0))
+                    {
+                        var handDistance = this.RHand.HandPoints[(int)HandPointIndex.HandCenter].z;
+                        lPower = Mathf.Min(this.maxDistance, handDistance);
+                    }
+                    this.RTire.AddForce(this.RTire.transform.forward * lPower * this.forceFactor);
+                    this.ShowAccelePower(this.RPowerText, lPower);
+                }
 
                 // for debug
                 this.RTire.AddForce(this.RTire.transform.forward * this.debugForceR);
